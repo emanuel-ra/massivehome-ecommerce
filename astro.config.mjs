7f08e985d14dc780a15fb096b92cc9f917338611
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-04-05 17:26:18 -0600

    ✨ Feat(home) Main page finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,60 @@
+import SpainFlag from '@/components/flags/Spain.astro';
+import UnitedStatesFlag from '@/components/flags/UnitedStates.astro';
+
+// Add missing imports
+export const LANGUAGES: Record<
+  string,
+  { code: string; name: string; flag: typeof SpainFlag }
+> = { 
+  en: {
+    code: 'en',
+    name: 'English',
+    flag: UnitedStatesFlag,
+  },
+  es: {
+    code: 'es',
+    name: 'Español',
+    flag: SpainFlag,
+  },
+};
+
+export const defaultLang = 'es';
+export const showDefaultLang = false;
+
+export const ui = {
+  es: {
+    'nav.home': 'Inicio!',
+    'nav.new': 'Nuevo!',
+    'nav.catagories': 'Categorías',
+    'nav.branches': 'Sucursales',
+    'nav.services': 'Servicios',
+    'nav.hot-sale': 'Liquidaciones',
+    'nav.cookies': 'Cookies',
+  },
+  en: {
+    'nav.en': 'Home',
+    'nav.new': 'New!',
+    'nav.catagories': 'Categories',
+    'nav.branches': 'Branches',
+    'nav.services': 'Services',
+    'nav.hot-sale': 'Hot Sale',
+    'nav.cookies': 'Cookies',
+  },
+} as const;
+
+export const routes = {
+  es: {
+    nuevo: 'nuevo',
+    categories: 'categorias',
+    branches: 'sucursales',
+    services: 'servicios',
+    'hot-sale': 'liquidaciones',
+  },
+  en: {
+    nuevo: 'new',
+    categories: 'categories',
+    branches: 'branches',
+    services: 'services',
+    'hot-sale': 'hot sale',
+  },
+};
